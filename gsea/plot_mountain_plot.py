423547e4c06ada4118d32b8a2f333288c9d33e4d
KwatME <kwatme8@gmail.com>
2018-01-11 11:35:03 -0800

    0.8

--- gsea/plot_mountain_plot.py
+++ gsea/plot_mountain_plot.py
@@ -19,7 +19,7 @@ def plot_mountain_plot(in_, cumulative_sums, score, title, file_path):
 
     ax = figure(figsize=(12, 8)).gca()
 
-    ax.plot(range(in_.size), in_, linewidth=1.8, color='#2E211B', alpha=0.8)
+    ax.plot(range(in_.size), in_, linewidth=0.8, color='#2E211B', alpha=0.8)
     ax.plot(range(in_.size), cumulative_sums, linewidth=2.6, color='#20D9BA')
 
     decorate(
