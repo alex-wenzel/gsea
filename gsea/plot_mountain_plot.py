2c65afdbb5ef1ad522e78e50ab6fca9b90fad75b
KwatME <kwatme8@gmail.com>
2018-01-11 11:31:49 -0800

    plot update

--- gsea/plot_mountain_plot.py
+++ gsea/plot_mountain_plot.py
@@ -4,13 +4,13 @@ from .plot.plot.decorate import decorate
 from .plot.plot.save_plot import save_plot
 
 
-def plot_mountain_plot(in_, y, cumulative_sums, title, file_path):
+def plot_mountain_plot(in_, cumulative_sums, score, title, file_path):
     """
     Plot mountain plot.
     Arguments:
         in_ (array):
-        y (array):
         cumulative_sums (array):
+        score (number):
         title (str):
         file_path (str):
     Returns:
@@ -19,11 +19,12 @@ def plot_mountain_plot(in_, y, cumulative_sums, title, file_path):
 
     ax = figure(figsize=(12, 8)).gca()
 
-    ax.plot(range(in_.size), in_, linewidth=1.8, color='#808080', alpha=0.16)
-    ax.plot(range(in_.size), y, linewidth=2.6, color='#9017E6')
-    ax.plot(range(in_.size), cumulative_sums, linewidth=1.8, color='#20D9BA')
+    ax.plot(range(in_.size), in_, linewidth=2.6, color='#808080', alpha=0.8)
+    ax.plot(range(in_.size), cumulative_sums, linewidth=2.6, color='#20D9BA')
 
-    decorate(title=title, legend_loc=None)
+    decorate(
+        title='{}\nEnrichment Score = {}'.format(title, score),
+        legend_loc=None)
 
     if file_path:
         save_plot(file_path)
